a8af616a95022f07483dff21a99523c91735238c
Aida Adalaber Kairosera <aidalbla@gmail.com>
2018-10-14 18:23:07 +0200

    feat(cypress): add some assertions for DOM

--- cypress/integration/app_actions.spec.js
+++ cypress/integration/app_actions.spec.js
@@ -5,6 +5,7 @@ context('Actions in the game', () => {
     it('shows the questions and the result of my answers and save them', ()=> {
         cy.wait(1000)
         cy.get('#btn-start').click();
+        cy.get('.intro-container').should('not.be.visible')
         cy.wait(1000)   
         cy.get('#item-1').click();
         cy.wait(1000)
@@ -22,9 +23,10 @@ context('Actions in the game', () => {
         cy.wait(1000)
         cy.get('#btn-next').click();
         cy.wait(1000)
-        cy.get('#player-name').type('Guybrush Threepwood');
+        cy.get('#player-name').type('Guybrush Threepwood').should('have.value', 'Guybrush Threepwood');
         cy.wait(1000)
         cy.get('#btn-send').click();
         cy.wait(1000)
+        cy.get('#score-container').should('be.visible');
     })
 })
